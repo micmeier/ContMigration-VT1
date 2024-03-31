ba6491c4a93dd8fb498ea6da2870f8c953ee3c39
Rinchen Kolodziejczyk <rinchen.kolo@gmail.com>
2024-03-31 20:28:02 +0000

    Dockerfile for CPU: 	- added default CMD cpu_sts: 	- modify yaml for better peformance and compatibility with Dockerfile setup: 	- General changes for smooth running flask_app: 	- added file for testing

--- cpu_intensive/setup_CPU.sh
+++ cpu_intensive/setup_CPU.sh
@@ -10,8 +10,18 @@ read -p "Enter int between 1-100%: " chosen_percentage
 echo "You entered: $chosen_percentage"
 
 # Update the YAML file with the chosen values
-sed -i "s/value: \"2\"/value: \"$chosen_cores\"/" cpu_sts.yaml
-sed -i "s/value: \"15\"/value: \"$chosen_percentage\"/" cpu_sts.yaml
+#sed -i "s/value: \"2\"/value: \"$chosen_cores\"/" cpu_sts.yaml
+#sed -i "s/value: \"15\"/value: \"$chosen_percentage\"/" cpu_sts.yaml
 
+#test
 # Run the yaml
 kubectl apply -f cpu_sts.yaml
+
+# Patch the StatefulSet with the chosen values
+kubectl patch statefulset cpu --type='json' -p "[{\"op\": \"replace\", \"path\": \"/spec/template/spec/containers/0/env/0/value\", \"value\": \"$chosen_cores\"}, {\"op\": \"replace\", \"path\": \"/spec/template/spec/containers/0/env/1/value\", \"value\": \"$chosen_percentage\"}]"
+
+
+
+
+# Run the yaml
+#kubectl apply -f cpu_sts.yaml
