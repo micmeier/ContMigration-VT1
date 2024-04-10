d4f5d62f9b11ad8d3368bd720387868493d7dcb0
Rinchen Kolodziejczyk <rinchen.kolo@gmail.com>
2024-04-10 19:58:25 +0000

    added flask app cpu-intensive app now pulled via local registry on each node

--- cpu_intensive/setup_CPU.sh
+++ cpu_intensive/setup_CPU.sh
@@ -2,26 +2,17 @@
 
 echo -e "How many cores should be utilized?"
 echo -e "If x exceeds available cores of host, the maximum no. of cores will be used."
-read -p " Choose between 1-x: " chosen_cores
-echo "You entered: $chosen_cores"
+read -p "Choose between 1-x: " chosen_cores
 
 echo -e "How much CPU usage per core?"
 read -p "Enter int between 1-100%: " chosen_percentage
-echo "You entered: $chosen_percentage"
 
-# Update the YAML file with the chosen values
-#sed -i "s/value: \"2\"/value: \"$chosen_cores\"/" cpu_sts.yaml
-#sed -i "s/value: \"15\"/value: \"$chosen_percentage\"/" cpu_sts.yaml
+# Number of Replicaset
+echo -e "Number of Replicasets?"
+read -p "Enter an int: " number_replicaset
 
-#test
 # Run the yaml
 kubectl apply -f cpu_sts.yaml
 
 # Patch the StatefulSet with the chosen values
-kubectl patch statefulset cpu --type='json' -p "[{\"op\": \"replace\", \"path\": \"/spec/template/spec/containers/0/env/0/value\", \"value\": \"$chosen_cores\"}, {\"op\": \"replace\", \"path\": \"/spec/template/spec/containers/0/env/1/value\", \"value\": \"$chosen_percentage\"}]"
-
-
-
-
-# Run the yaml
-#kubectl apply -f cpu_sts.yaml
+kubectl patch statefulset cpu --type='json' -p "[{\"op\": \"replace\", \"path\": \"/spec/template/spec/containers/0/env/0/value\", \"value\": \"$chosen_cores\"}, {\"op\": \"replace\", \"path\": \"/spec/template/spec/containers/0/env/1/value\", \"value\": \"$chosen_percentage\"}, {\"op\": \"replace\", \"path\": \"/spec/replicas\", \"value\": $number_replicaset}]"
