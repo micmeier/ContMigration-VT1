cbd3c7e44ebad3703729c355d15951855abdd912
micmeier <michaelazhari.meier@protonmail.ch>
2024-10-21 15:57:37 +0200

    Added migration api app

---
+++ apps/migration_api/migration.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, HTTPException
+import subprocess
+
+app = FastAPI()
+
+@app.post("/trigger-migration/")
+async def trigger_migration():
+    try:
+        # Run the migration script located at /host/migration.sh
+        #result = subprocess.run(["/home/ubuntu/ContMigration/scripts/migration/single-migration.sh"], check=True, capture_output=True, text=True)
+        result = subprocess.run(["../../scripts/migration/single-migration.sh"], check=True, capture_output=True, text=True)
+
+        # Return the result of the script execution
+        return {"message": "Migration script executed successfully", "output": result.stdout}
+
+    except subprocess.CalledProcessError as e:
+        # Handle errors if the script fails
+        raise HTTPException(status_code=500, detail=f"Error executing script: {e.stderr}")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
