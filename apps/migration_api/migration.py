8fcc7b311e0fb63f5c6f3470537210435fa33397
micmeier <michaelazhari.meier@protonmail.ch>
2024-10-27 23:33:04 +0100

    Migrate based on rules

--- apps/migration_api/migration.py
+++ apps/migration_api/migration.py
@@ -3,23 +3,36 @@ import subprocess
 
 app = FastAPI()
 
-@app.post("/trigger-migration/")
-async def trigger_migration(request: Request):
-    body = await request.json()
-    print(body)
+ruleToTriggerMigration = [
+    'Read sensitive file untrusted'
+]
 
+@app.post("/trigger-migration/")
+async def handle_warning(request: Request):
+    body = await request.json()
+    hostname = body.get("hostname")
+    rule = body.get("rule")
+    priority = body.get("priority")
+    k8s_pod_name = body.get("output_fields", {}).get("k8s.pod.name")
+
+    if rule in ruleToTriggerMigration:
+        print(f"Security event with priority {priority}and rule {rule} received from {hostname}")
+        print(f"Migrating pod {k8s_pod_name} to a secure cluster")
+        return await trigger_migration()
+
+
+async def trigger_migration(k8s_pod_name):
     try:
         # Run the migration script located at /host/migration.sh
         #result = subprocess.run(["/home/ubuntu/ContMigration/scripts/migration/single-migration.sh"], check=True, capture_output=True, text=True)
-        result = subprocess.run(["../../scripts/migration/single-migration.sh", "nginx-7bf5d9d764-42gms"], check=True, capture_output=True, text=True)
+        result = subprocess.run(["../../scripts/migration/single-migration.sh", k8s_pod_name], check=True, capture_output=True, text=True)
 
         # Return the result of the script execution
         return {"message": "Migration script executed successfully", "output": result.stdout}
 
     except subprocess.CalledProcessError as e:
-        # Handle errors if the script fails
+    # Handle errors if the script fails
         raise HTTPException(status_code=500, detail=f"Error executing script: {e.stderr}")
-
 
 
 if __name__ == "__main__":
