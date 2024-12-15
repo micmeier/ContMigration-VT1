092b7272c677a4f29352f97be62ce77ab88438c2
micmeier <michaelazhari.meier@protonmail.ch>
2024-12-15 22:26:42 +0100

    Improve script

--- scripts/utils/forensic_analysis/forensic_analysis.sh
+++ scripts/utils/forensic_analysis/forensic_analysis.sh
@@ -14,8 +14,7 @@ TMP_DIR="$OUTPUT_DIR/temp"
 # Step 1: Prepare environment
 prepare_environment() {
     echo "Preparing environment..."
-    mkdir -p "$TMP_DIR"
-    mkdir -p "$OUTPUT_DIR"
+    mkdir -p "$TMP_DIR/changed"
     echo "Environment prepared."
 }
 
@@ -25,40 +24,34 @@ inspect_checkpoint() {
         echo "Error: Checkpoint file $CHECKPOINT_FILE does not exist."
         exit 1
     fi
-    echo "Getting checkpoint information"
+    echo "Generating forensic report..."
     echo "----- Checkpoint information -----" >> "$OUTPUT_DIR/forensic_report.txt"
     sudo checkpointctl inspect "$CHECKPOINT_FILE" >> "$OUTPUT_DIR/forensic_report.txt" || {
         echo "Error: Failed to extract process list."
         exit 1
     }
-    echo "Checkpoint information saved to $OUTPUT_DIR/forensic_report.txt."
 }
 
 analyze_process_tree() {
-    echo "Extracting process list..."
     echo "----- Process tree -----" >> "$OUTPUT_DIR/forensic_report.txt"
     sudo checkpointctl inspect "$CHECKPOINT_FILE" --ps-tree-cmd | awk '/Process tree/{flag=1} flag' >> "$OUTPUT_DIR/forensic_report.txt" || {
         echo "Error extracting process tree from checkpoint file"
         exit 1
     }
-    echo "Process list saved to $OUTPUT_DIR/forensic_report.txt."
 }
 
 
 # Step 3: Analyze checkpoint data
 # This function extracts filesystem metadata from the checkpoint file and saves it to the output directory.
 analyze_sockets() {
-    echo "Extracting socket list"
     echo "----- Open sockets -----" >> "$OUTPUT_DIR/forensic_report.txt"
     sudo checkpointctl inspect "$CHECKPOINT_FILE" --sockets | awk '/Process tree/{flag=1; next} flag' >> "$OUTPUT_DIR/forensic_report.txt" || {
         echo "Error extracting process tree from checkpoint file"
         exit 1
     }
-    echo "Open socket saved to $OUTPUT_DIR/forensic_report.txt."
 }
 
 analyze_files() {
-    echo "Extracting file list"
     echo "----- Open files -----" >> "$OUTPUT_DIR/forensic_report.txt"
     sudo checkpointctl inspect "$CHECKPOINT_FILE" --files | awk '/Open files/{flag=1; next} flag' >> "$OUTPUT_DIR/forensic_report.txt" || {
         echo "Error extracting process tree from checkpoint file"
@@ -67,8 +60,15 @@ analyze_files() {
     echo "Open files saved to $OUTPUT_DIR/forensic_report.txt."
 }
 
+analyze_memory() {
+    echo "----- Memory size -----" >> "$OUTPUT_DIR/forensic_report.txt"
+    sudo checkpointctl memparse "$CHECKPOINT_FILE" | awk '/Displaying processes/{flag=1; next} flag' >> "$OUTPUT_DIR/forensic_report.txt" || {
+        echo "Error extracting memory size from checkpoint file"
+        exit 1
+    }
+}
+
 extract_checkpoint() {
-    echo "Extracting container checkpoint..."
     tar -xf "$CHECKPOINT_FILE" -C "$TMP_DIR" || {
         echo "Error: Failed to extract checkpoint file."
         exit 1
@@ -79,7 +79,6 @@ extract_checkpoint() {
         echo "Error: Failed to extract rootfs-diff file."
         exit 1
     }
-    echo "Checkpoint extracted to $TMP_DIR."
 }
 
 # Main function to run the script
@@ -88,6 +87,7 @@ main() {
     inspect_checkpoint
     analyze_process_tree
     analyze_sockets
+    analyze_memory
 #    analyze_files
     extract_checkpoint
 }
